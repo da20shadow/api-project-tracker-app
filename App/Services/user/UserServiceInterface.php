f22bc6492f34b24f83307de885f9fedf3a6b0610
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-07-03 19:47:14 +0300

    Updated UserServiceinterface
    Added methods for getting user by id username and email

--- App/Services/user/UserServiceInterface.php
+++ App/Services/user/UserServiceInterface.php
@@ -5,8 +5,11 @@ namespace App\Services\user;
 interface UserServiceInterface
 {
     public function create($userInputs);
+    public function login($userInputs);
     public function update($userInputs);
     public function delete($userInputs);
-    public function getUserById($userInputs);
+    public function getUserById($user_id);
+    public function getUserByUsername($username);
+    public function getUserByEmail($email);
     public function createUserDTO($userInputs);
 }
