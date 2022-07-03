a2692e77db25777f1552e74febe97eeec97fd2d0
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-07-03 19:44:42 +0300

    Added getters in UserDTO

--- App/Models/user/UserDTO.php
+++ App/Models/user/UserDTO.php
@@ -29,6 +29,64 @@ class UserDTO
             ->setRole($role);
     }
 
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRole(): string
+    {
+        return $this->role;
+    }
+
+
+
     public function setId(int $id): static
     {
         $this->id = $id;
