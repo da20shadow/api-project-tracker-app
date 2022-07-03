62e5fe26f7ca86b24a3d83736d90c5f880f1388e
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-07-03 18:12:40 +0300

    Added methods in UserRepositoryInterface

--- App/Repositories/user/UserRepositoryInterface.php
+++ App/Repositories/user/UserRepositoryInterface.php
@@ -2,7 +2,14 @@
 
 namespace App\Repositories\user;
 
+use App\Models\user\UserDTO;
+
 interface UserRepositoryInterface
 {
-
+    public function insert(UserDTO $userDTO);
+    public function update(UserDTO $userDTO);
+    public function getUserById($user_id);
+    public function getUserByUsername($username);
+    public function getUserByEmail($email);
+    public function delete(UserDTO $userDTO);
 }
