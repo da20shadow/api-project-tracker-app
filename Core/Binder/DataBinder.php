0e66b7af582a1f9a748df0a0c838bb88517f772a
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-07-03 21:24:27 +0300

    Databinder added

--- Core/Binder/DataBinder.php
+++ Core/Binder/DataBinder.php
@@ -4,5 +4,20 @@ namespace Core\Binder;
 
 class DataBinder
 {
+    public function bind($formData, $className): mixed
+    {
+        $object = new $className();
 
+        foreach ($formData as $key => $value) {
+            $methodName = "set" . implode("",
+                    array_map(function ($el) {
+                        return ucfirst($el);
+                    }, explode("_", $key)));
+
+            if (method_exists($object, $methodName)) {
+                $object->$methodName($value);
+            }
+        }
+        return $object;
+    }
 }
