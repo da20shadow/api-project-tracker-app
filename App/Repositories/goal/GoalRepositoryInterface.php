866f498156e5cc17f630551e6a45cba5ca81cfe3
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-07-04 09:23:46 +0300

    Added methods in GoalRepositoryInterface

--- App/Repositories/goal/GoalRepositoryInterface.php
+++ App/Repositories/goal/GoalRepositoryInterface.php
@@ -2,7 +2,16 @@
 
 namespace App\Repositories\goal;
 
+use App\Models\goal\GoalDTO;
+use Generator;
+
 interface GoalRepositoryInterface
 {
-
+    public function insert(GoalDTO $goalDTO): bool;
+    public function updateTitle(GoalDTO $goalDTO): bool;
+    public function updateDescription(GoalDTO $goalDTO): bool;
+    public function updateDueDate(GoalDTO $goalDTO): bool;
+    public function delete(GoalDTO $goalDTO): bool;
+    public function getGoalById(int $goal_id): GoalDTO;
+    public function getGoalsByUserId(int $user_id): Generator;
 }
