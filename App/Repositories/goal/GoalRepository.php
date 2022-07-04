1fadfafde2afab1e5d4d7d9a0b4d5afe9dba3ae8
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-07-04 10:00:53 +0300

    Added insert new goal method

--- App/Repositories/goal/GoalRepository.php
+++ App/Repositories/goal/GoalRepository.php
@@ -3,14 +3,40 @@
 namespace App\Repositories\goal;
 
 use App\Models\goal\GoalDTO;
+use Database\DBConnector;
+use Database\PDODatabase;
 use Generator;
+use PDOException;
 
 class GoalRepository implements GoalRepositoryInterface
 {
+    private PDODatabase $db;
+
+    public function __construct()
+    {
+        $this->db = DBConnector::create();
+    }
 
     public function insert(GoalDTO $goalDTO): bool
     {
-        // TODO: Implement insert() method.
+        try {
+            $this->db->query("
+                INSERT INTO goals
+                (goal_title,goal_description,due_date,user_id,goal_category)
+                VALUES (:title,:description,:due_date,:user_id,:category)
+            ")->execute(array(
+                'title' => $goalDTO->getTitle(),
+                'description' => $goalDTO->getDescription(),
+                'due_date' => $goalDTO->getDueDate(),
+                'user_id' => $goalDTO->getUserId(),
+                ':category' => $goalDTO->getCategory()
+            ));
+            return true;
+        } catch (PDOException $PDOException) {
+            $pdoError = $PDOException->getMessage();
+            //TODO log the error
+            return false;
+        }
     }
 
     public function updateTitle(GoalDTO $goalDTO): bool
