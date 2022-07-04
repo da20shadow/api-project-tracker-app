70d501e861c69d6034000609e28a3598ed2cad26
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-07-04 09:24:01 +0300

    Added methods in goalService interfacew

--- App/Services/goal/GoalServiceInterface.php
+++ App/Services/goal/GoalServiceInterface.php
@@ -4,5 +4,11 @@ namespace App\Services\goal;
 
 interface GoalServiceInterface
 {
-
+    public function create(array $userInputs,$userInfo);
+    public function updateTitle(array $userInputs,$userInfo);
+    public function updateDescription(array $userInputs,$userInfo);
+    public function updateDueDate(array $userInputs,$userInfo);
+    public function delete(array $userInputs,$userInfo);
+    public function getGoalById(array $userInputs,$userInfo);
+    public function getUserGoalsByUserId(array $userInputs,$userInfo);
 }
