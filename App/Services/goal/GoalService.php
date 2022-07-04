3a96fa5733dfbb1662c01ee60a72ba0031d03e24
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-07-04 10:00:36 +0300

    Method create new goal done

--- App/Services/goal/GoalService.php
+++ App/Services/goal/GoalService.php
@@ -2,8 +2,10 @@
 
 namespace App\Services\goal;
 
+use App\Models\goal\GoalDTO;
 use App\Repositories\goal\GoalRepository;
 use App\Repositories\goal\GoalRepositoryInterface;
+use Exception;
 
 class GoalService implements GoalServiceInterface
 {
@@ -17,7 +19,44 @@ class GoalService implements GoalServiceInterface
     /** ----------------CREATE-------------------- */
     public function create(array $userInputs,$userInfo)
     {
-        // TODO: Implement create() method.
+        if (!isset($userInputs['title']) || !isset($userInputs['description'])
+            || !isset($userInputs['category']) || !isset($userInputs['user_id'])
+            || !isset($userInputs['due_date']) || $userInputs['user_id'] != $userInfo['id'])
+        {
+            http_response_code(403);
+            echo json_encode([
+                'message' => 'All fields are required!'
+            ],JSON_PRETTY_PRINT);
+        }
+
+        $title = $userInputs['title'];
+        $description = $userInputs['description'];
+        $category = $userInputs['category'];
+        $userId = $userInputs['user_id'];
+        $dueDate = $userInputs['due_date'];
+
+        $goal = null;
+        try {
+            $goal = GoalDTO::create($title, $description, $category, $userId, $dueDate);
+        } catch (Exception $e) {
+            echo json_encode($e->getMessage(), JSON_PRETTY_PRINT);
+        }
+
+        $result = $this->goalRepository->insert($goal);
+
+        if (!$result) {
+            http_response_code(403);
+            echo json_encode([
+                'message' => 'All fields are required!'
+            ],JSON_PRETTY_PRINT);
+            return;
+        }
+
+        http_response_code(200);
+        echo json_encode([
+            'message' => 'Successfully Created New Goal!'
+        ],JSON_PRETTY_PRINT);
+
     }
 
     /** ----------------UPDATE-------------------- */
