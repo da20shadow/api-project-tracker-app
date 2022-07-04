79eb388cb2c788e092506de4bac2eda4a96886f5
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-07-04 10:47:28 +0300

    added array as expected userInfo

--- App/Services/goal/GoalServiceInterface.php
+++ App/Services/goal/GoalServiceInterface.php
@@ -4,11 +4,12 @@ namespace App\Services\goal;
 
 interface GoalServiceInterface
 {
-    public function create(array $userInputs,$userInfo);
-    public function updateTitle(array $userInputs,$userInfo);
-    public function updateDescription(array $userInputs,$userInfo);
-    public function updateDueDate(array $userInputs,$userInfo);
-    public function delete(array $userInputs,$userInfo);
-    public function getGoalById(array $userInputs,$userInfo);
-    public function getUserGoalsByUserId(array $userInputs,$userInfo);
+    public function create(array $userInputs, array $userInfo);
+    public function updateTitle(array $userInputs, array $userInfo);
+    public function updateDescription(array $userInputs, array $userInfo);
+    public function updateCategory(array $userInputs, array $userInfo);
+    public function updateDueDate(array $userInputs, array $userInfo);
+    public function delete(array $userInputs, array $userInfo);
+    public function getGoalById(array $userInputs, array $userInfo);
+    public function getUserGoalsByUserId(array $userInputs, array $userInfo);
 }
